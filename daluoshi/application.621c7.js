b3b8d7f56fe3dea9f1ad27a5b7eb3dcd152bd346
zfs533 <2415933434@qq.com>
2026-09-24 10:14:40 +0800

    打螺丝更新

--- daluoshi/application.621c7.js
+++ daluoshi/application.621c7.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.23847.json';
+          this.settingsPath = 'src/settings.8f474.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
--- daluoshi/index.8a80d.js
+++ daluoshi/index.8a80d.js
@@ -1,4 +1,4 @@
-System.register(["./application.2836f.js"], function (_export, _context) {
+System.register(["./application.621c7.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
