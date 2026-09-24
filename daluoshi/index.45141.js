8dd8a18148b1e351c1a45a92db80400f5688421d
zfs533 <2415933434@qq.com>
2026-09-24 10:16:29 +0800

    打螺丝更新

--- daluoshi/index.45141.js
+++ daluoshi/index.45141.js
@@ -1,4 +1,4 @@
-System.register(["./application.621c7.js"], function (_export, _context) {
+System.register(["./application.d42e5.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
